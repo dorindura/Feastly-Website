a8a4f382f8e1eabcd5bc6573d3c9b9a1c1a47c2a
Dorin Dura <duradorin11@yahoo.com>
2024-08-07 13:35:01 +0300

    Lastly-Changes-Entire-Project

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -8,7 +8,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 const placeOrder = async (req, res) => {
 
-    const frontend_url = "https://feastly-website-frontend.onrender.com";
+    const frontend_url = "https://www.feastly-delivery.com/";
 
     try {
         const newOrder = new orderModel({
